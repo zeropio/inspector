f4026423f5b90940fb764afe15e41685606a1083
zeropio <emilio.sanchez@outlook.es>
2024-02-22 14:11:24 +0100

    Add user parsing

--- src/process.rs
+++ src/process.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use lazy_static::lazy_static;
 use parking_lot::Mutex;
 use std::collections::VecDeque;
-use crate::utils::cat;
+use crate::utils::{cat, process_search_line, get_username_from_uid};
 
 // Create global structure
 struct ProcessInfo {
@@ -34,23 +34,49 @@ fn check_proc(path: &PathBuf) -> bool {
 
 // Parser proc
 fn parse_proc(path: &PathBuf) {
+    // Placeholder: Implement logic to get user, cpu_usage, mem_usage, and command
+    let pid: i32;
+    let user: String;
+    let user = "placeholder_user".to_string(); // Replace this with actual logic
+    let cpu_usage = 0.0; // Placeholder
+    let mem_usage = 0.0; // Placeholder
+    let command = "placeholder_command".to_string(); // Placeholder
+
+    // Get PD
     // Attempt to extract the file name as a string and parse it as i32
     let file_name = match path.file_name().and_then(|n| n.to_str()) {
         Some(name) => name,
         None => return, // Early return if file_name is not found or not a valid UTF-8 string
     };
 
-    let pid = match file_name.parse::<i32>() {
+    pid = match file_name.parse::<i32>() {
         Ok(pid) => pid,
         Err(_) => return, // Early return if file_name cannot be parsed as i32
     };
 
-    // At this point, you have a valid PID
-    // Placeholder: Implement logic to get user, cpu_usage, mem_usage, and command
-    let user = "placeholder_user".to_string(); // Replace this with actual logic
-    let cpu_usage = 0.0; // Placeholder
-    let mem_usage = 0.0; // Placeholder
-    let command = "placeholder_command".to_string(); // Placeholder
+    // Get username
+    let status_path = path.join("status");
+    let mut uid_str: String = String::new();
+
+    match cat(&status_path) {
+        Ok(content) => {
+            uid_str = process_search_line(&content, "Uid");
+        },
+        Err(e) => {
+            println!("Error reading file: {}", e);
+            // uid_str already initialized to an empty string, so no need to reassign
+        },
+    }
+
+    match uid_str.parse::<u32>() {
+        Ok(uid) => {
+            match get_username_from_uid(uid) {
+                Some(username) => user = username,
+                None => println!("No user found for UID {}", uid),
+            }
+        },
+        Err(_) => println!("Failed to parse UID from string: {}", uid_str),
+    }
 
     // Add process info
     add_process_info(ProcessInfo {
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, Read}; // Import the `io` module and `Read` trait
 use std::path::Path;
+use nix::unistd::User;
 
 // A simple implementation of `% cat path`
 pub fn cat(path: &Path) -> io::Result<String> {
@@ -8,4 +9,26 @@ pub fn cat(path: &Path) -> io::Result<String> {
     let mut s = String::new();
     f.read_to_string(&mut s)?;
     Ok(s)
+}
+
+// Find str in line from file
+pub fn process_search_line(content: &str, search: &str) -> String {
+    for line in content.lines() {
+        if line.starts_with(search) {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() > 1 {
+                return parts[1].to_string(); // Convert to String
+            }
+            break;
+        }
+    }
+    String::new() // Return an empty string if not found
+}
+
+// Get username
+pub fn get_username_from_uid(uid: u32) -> Option<String> {
+    match User::from_uid(nix::unistd::Uid::from_raw(uid)) {
+        Ok(Some(user)) => Some(user.name),
+        _ => None,
+    }
 }
