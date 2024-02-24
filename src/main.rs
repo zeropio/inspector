0983ec69a89a6944d0c3c4c00924f7954c5571b7
zeropio <emilio.sanchez@outlook.es>
2024-02-24 22:55:11 +0100

    Update Monitoring

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use crate::process::{access_proc, get_all_process_info};
+use std::{thread, time::Duration};
+
+mod process;
+mod utils;
+
+fn main() {
+
+    let update_interval = Duration::from_secs(5);
+
+    // Monitor
+    loop {
+        access_proc();
+        let processes = get_all_process_info();
+
+        // Testing information returned
+        for process in processes {
+            println!("PID: {}, User: {}, Command: {}, CPU Usage: {}, \
+            Memory Usage: {}", process.pid(), process.user(), process.command(),
+                                process.cpu_usage(), process.mem_usage());
+        }
+
+        // Pause the loop for the specified update interval
+        thread::sleep(update_interval);
+    }
+}
